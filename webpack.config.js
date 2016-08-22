78d0a58be604786425cabbc69a0e6f6752f1e5f9
Roger Chen <rogerchen@bluetechnology.com.tw>
2016-08-22 11:10:51 +0800

    WEBPACK SETTING

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ plugins.push(new webpack.ProvidePlugin({
 }));
 
  
-plugins.push(new webpack.optimize.UglifyJsPlugin({ exclude: /\.min\.js$/ , minimize: true }));
+//plugins.push(new webpack.optimize.UglifyJsPlugin({ exclude: /\.min\.js$/ , minimize: true }));
 
 console.log();
 module.exports = {
@@ -24,19 +24,21 @@ module.exports = {
     entry: {"vendors":__dirname+ "/src/config/vendors.js",'style':__dirname+ "/src/config/style.js"},
     output: {
         path: "./production/js/",
-        filename:  "[name].js",
-        
+        filename:  "[name].js",  
+        publicPath:'./js/'      
     },
     devtool:'source-map',
     module: {
         loaders: [
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=100000' },            
             { test: /\.css$/, loader: "style!css" },
             {
              test: /\.scss$/,
              loaders: ["style", "css", "sass"]
             },
-            { test: /\.(woff|svg|ttf|eot)([\?]?.*)$/, loader: "file-loader?name=[name].[ext]" }
+            { test: /\.(png|jpg|woff|svg|ttf|eot)([\?]?.*)$/, loader: "file-loader?name=[name].[ext]" }
         ]
     },
     plugins: plugins
+    
 };
