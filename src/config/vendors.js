fc5f5d7070bf6b464c848e0a97320240c919108d
Roger Chen <rogerchen@bluetechnology.com.tw>
2016-08-22 10:45:54 +0800

    setting for webpack

--- src/config/vendors.js
+++ src/config/vendors.js
@@ -1,41 +1,41 @@
 
 
- require('../../vendors/jquery/dist/jquery.min.js');
- require('../../vendors/bootstrap/dist/js/bootstrap.min.js');
-    require('../../vendors/fastclick/lib/fastclick.js');
-    require('../../vendors/nprogress/nprogress.js');
-    require('bootstrap-daterangepicker');
-    require('../../vendors/bootstrap-progressbar/bootstrap-progressbar.js');
-    // require('../../vendors/Chart.js/dist/Chart.min.js');
-    require('../../vendors/gauge.js/dist/gauge.min.js');
+require('../../vendors/jquery/dist/jquery.min.js');
+require('../../vendors/bootstrap/dist/js/bootstrap.min.js');
+require('../../vendors/fastclick/lib/fastclick.js');
+require('../../vendors/nprogress/nprogress.js');
+require('bootstrap-daterangepicker');
+require('../../vendors/bootstrap-progressbar/bootstrap-progressbar.js');
+// require('../../vendors/Chart.js/dist/Chart.min.js');
+require('../../vendors/gauge.js/dist/gauge.min.js');
 
-    require('../../vendors/iCheck/icheck.min.js');
-    require('../../vendors/skycons/skycons.js');
+require('../../vendors/iCheck/icheck.min.js');
+require('../../vendors/skycons/skycons.js');
 
-    // require('../../vendors/Flot/jquery.flot.js');
-    // require('../../vendors/Flot/jquery.flot.pie.js');
-    // require('../../vendors/Flot/jquery.flot.time.js');
-    // require('../../vendors/Flot/jquery.flot.stack.js');
-    // require('../../vendors/Flot/jquery.flot.resize.js');
+// require('../../vendors/Flot/jquery.flot.js');
+// require('../../vendors/Flot/jquery.flot.pie.js');
+// require('../../vendors/Flot/jquery.flot.time.js');
+// require('../../vendors/Flot/jquery.flot.stack.js');
+// require('../../vendors/Flot/jquery.flot.resize.js');
 
-    // require('../../vendors/flot.orderbars/js/jquery.flot.orderBars.js');
-    // require('../../vendors/flot-spline/js/jquery.flot.spline.min.js');
-    // require('../../vendors/flot.curvedlines/curvedLines.js');
+// require('../../vendors/flot.orderbars/js/jquery.flot.orderBars.js');
+// require('../../vendors/flot-spline/js/jquery.flot.spline.min.js');
+// require('../../vendors/flot.curvedlines/curvedLines.js');
 
-    // require('../../vendors/DateJS/build/date.js');
+// require('../../vendors/DateJS/build/date.js');
 
-    require('../../vendors/jqvmap/dist/jquery.vmap.js');
-    require('../../vendors/jqvmap/dist/maps/jquery.vmap.world.js');
-    require('../../vendors/jqvmap/examples/js/jquery.vmap.sampledata.js');
+require('../../vendors/jqvmap/dist/jquery.vmap.js');
+require('../../vendors/jqvmap/dist/maps/jquery.vmap.world.js');
+require('../../vendors/jqvmap/examples/js/jquery.vmap.sampledata.js');
 
- 
-    require('../../vendors/bootstrap-daterangepicker/daterangepicker.js');
- 
-    require('../../build/js/custom.min.js');
-   //     require('../js/helpers/smartresize.js');
-    //require('../js/custom.js');
-     require('angular');
-    // require('angular-route');
-     require('angular-resource');
+
+require('../../vendors/bootstrap-daterangepicker/daterangepicker.js');
+
+require('../../build/js/custom.min.js');
+//     require('../js/helpers/smartresize.js');
+//require('../js/custom.js');
+require('angular');
+require('../../vendors/angular-route/angular-route.min.js');
+require('angular-resource');
       //require('../../src/scss/custom.scss');
       //@import "../../src/scss/custom.scss";
