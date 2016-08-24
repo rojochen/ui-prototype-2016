e55aa84c625171266d1d51572da936f154ead927
Louis <neighborhood0830@gmail.com>
2016-08-24 11:05:48 +0800

    add bootstrap-switch

--- src/config/vendors.js
+++ src/config/vendors.js
@@ -1,6 +1,5 @@
-
 var $ = require('../../vendors/jquery/dist/jquery.min.js');
- 
+
 require('../../vendors/jquery/dist/jquery.min.js');
 require('../../vendors/bootstrap/dist/js/bootstrap.min.js');
 require('../../vendors/fastclick/lib/fastclick.js');
@@ -33,12 +32,13 @@ require('../../vendors/jqvmap/examples/js/jquery.vmap.sampledata.js');
 require('../../vendors/bootstrap-daterangepicker/daterangepicker.js');
 
 require('../../vendors/blockUI/jquery.blockUI.js');
-
-// require('../../build/js/custom.min.js');
+//  bootstrap-switch
+require('../../vendors/bootstrap-switch/dist/js/bootstrap-switch.min.js');
+    // require('../../build/js/custom.min.js');
 require('angular');
 require('../../vendors/angular-route/angular-route.min.js');
 require('angular-resource');
 require('../js/helpers/smartresize.js');
 require('../js/custom.js');
 
-require('../js/page/blockUI.js'); 
+require('../js/page/blockUI.js');
