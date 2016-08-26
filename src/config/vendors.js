2418166585fe5523e9e0f3d8c828952068deabea
Roger Chen <rogerchen@bluetechnology.com.tw>
2016-08-26 18:40:36 +0800

    add arg for webapck

--- src/config/vendors.js
+++ src/config/vendors.js
@@ -8,7 +8,7 @@ require('../../vendors/nprogress/nprogress.js');
 require('bootstrap-daterangepicker');
 require('../../vendors/bootstrap-progressbar/bootstrap-progressbar.js');
 // require('../../vendors/Chart.js/dist/Chart.min.js');
-require('../../vendors/gauge.js/dist/gauge.min.js');
+//require('../../vendors/gauge.js/dist/gauge.min.js');
 
 require('../../vendors/iCheck/icheck.min.js');
 require('../../vendors/skycons/skycons.js');
@@ -25,22 +25,22 @@ require('../../vendors/skycons/skycons.js');
 
 // require('../../vendors/DateJS/build/date.js');
 
-require('../../vendors/jqvmap/dist/jquery.vmap.js');
-require('../../vendors/jqvmap/dist/maps/jquery.vmap.world.js');
-require('../../vendors/jqvmap/examples/js/jquery.vmap.sampledata.js');
+// require('../../vendors/jqvmap/dist/jquery.vmap.js');
+// require('../../vendors/jqvmap/dist/maps/jquery.vmap.world.js');
+// require('../../vendors/jqvmap/examples/js/jquery.vmap.sampledata.js');
 
 
 require('../../vendors/bootstrap-daterangepicker/daterangepicker.js');
 
-require('../../vendors/blockUI/jquery.blockUI.js');
-require('../../vendors/echarts/dist/echarts.min.js');
-
+// require('../../vendors/blockUI/jquery.blockUI.js');
+// require('../../vendors/echarts/dist/echarts.min.js');
+//remove [name].js
 // require('../../build/js/custom.min.js');
-require('angular');
-require('../../vendors/angular-route/angular-route.min.js');
-require('angular-resource');
-require('../js/helpers/smartresize.js');
-require('../js/custom.js');
+// require('angular');
+// require('../../vendors/angular-route/angular-route.min.js');
+// require('angular-resource');
+// require('../js/helpers/smartresize.js');
+// require('../js/custom.js');
 
-require('../js/page/blockUI.js'); 
-require('../js/page/echarts.js'); 
+// require('../js/page/blockUI.js'); 
+// require('../js/page/echarts.js'); 
