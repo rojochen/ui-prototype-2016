573f5464871ccd70afac41ae24968c4f1ace28df
Louis <neighborhood0830@gmail.com>
2016-08-28 17:29:05 +0800

    icheck-background Error

--- src/config/louis.js
+++ src/config/louis.js
@@ -1,2 +1,5 @@
+// js
 require('../../vendors/bootstrap-switch/dist/js/bootstrap-switch.min.js');
 require('../js/page/projects.js');
+// css
+require("../../vendors/iCheck/skins/flat/green.css");
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,8 @@ plugins.push(new webpack.ProvidePlugin({
 }));
 
 
-plugins.push(new webpack.optimize.UglifyJsPlugin({ exclude: /\.min\.js$/, minimize: true }));
+// plugins.push(new webpack.optimize.UglifyJsPlugin({ exclude: /\.min\.js$/, minimize: true }));
+plugins.push(new webpack.optimize.UglifyJsPlugin({ exclude: /png|jpg|gif｜\.min\.js$/, minimize: true }));
 
 var entry = {};
 if (!css) {
