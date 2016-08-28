01e256140524b7e6213a31288f98875c11f68a4e
Louis <neighborhood0830@gmail.com>
2016-08-28 16:50:55 +0800

    debug louis路徑名稱錯誤

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var jquery = require('./vendors/jquery/dist/jquery.min.js');
 var css = process.argv.indexOf('--css') === -1;
 var vendors = process.argv.indexOf('--vendors') === -1;
 var joe = process.argv.indexOf('--joe') === -1;
-var louis =process.argv.indexOf('--louse') === -1;
+var louis = process.argv.indexOf('--louis') === -1;
 
 plugins.push(new BowerWebpackPlugin({
     modulesDirectories: ["vendors"],
@@ -14,52 +14,51 @@ plugins.push(new BowerWebpackPlugin({
     excludes: /.*\.less/
 }));
 plugins.push(new webpack.ProvidePlugin({
-        'window.jQuery': 'jquery',
-        'window.$': 'jquery',
-        jQuery:'jquery',
-        $:'jquery'
+    'window.jQuery': 'jquery',
+    'window.$': 'jquery',
+    jQuery: 'jquery',
+    $: 'jquery'
 }));
 
- 
-plugins.push(new webpack.optimize.UglifyJsPlugin({ exclude: /\.min\.js$/ , minimize: true }));
+
+plugins.push(new webpack.optimize.UglifyJsPlugin({ exclude: /\.min\.js$/, minimize: true }));
 
 var entry = {};
-if(!css){
+if (!css) {
     //"js/vendors":__dirname+ "/src/config/vendors.js",'css/style':__dirname+ "/src/config/style.js"
-    
-     entry["css/style"] = __dirname+ "/src/config/style.js";
+
+    entry["css/style"] = __dirname + "/src/config/style.js";
 }
-if(!vendors){
-  entry["js/vendors"] = __dirname+ "/src/config/vendors.js";
+if (!vendors) {
+    entry["js/vendors"] = __dirname + "/src/config/vendors.js";
 }
 
-if(!joe){
-   // {"js/joe":__dirname+ "/src/config/joe.js" ,"js/louis":__dirname+ "/src/config/louis.js"};
-   entry["js/joe"] = __dirname+ "/src/config/joe.js";
+if (!joe) {
+    // {"js/joe":__dirname+ "/src/config/joe.js" ,"js/louis":__dirname+ "/src/config/louis.js"};
+    entry["js/joe"] = __dirname + "/src/config/joe.js";
 }
-if(!louis){
-    entry["js/louis"] = __dirname+ "/src/config/louis.js";
+if (!louis) {
+    entry["js/louis"] = __dirname + "/src/config/louis.js";
 }
 module.exports = {
-    context: __dirname ,
+    context: __dirname,
     entry: entry,
     output: {
         path: "./production/assets/",
-        filename:  "[name].js",  
-        publicPath:'./assets/'      
+        filename: "[name].js",
+        publicPath: './assets/'
     },
-    devtool:'source-map',
+    devtool: 'source-map',
     module: {
         loaders: [
-            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=100000' },            
-            { test: /\.css$/, loader: "style!css" },
-            {
-             test: /\.scss$/,
-             loaders: ["style", "css", "sass"]
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=100000' },
+            { test: /\.css$/, loader: "style!css" }, {
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
             },
             { test: /\.(png|jpg|woff|svg|ttf|eot)([\?]?.*)$/, loader: "file-loader?name=img/[name].[ext]" }
         ]
     },
     plugins: plugins
-    
+
 };
