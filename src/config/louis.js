f000a73f4ce9768bbab342a53192697b28a000e5
Louis <neighborhood0830@gmail.com>
2016-08-30 17:35:38 +0800

    error

--- src/config/louis.js
+++ src/config/louis.js
@@ -1,8 +1,8 @@
-var $ =require('../../vendors/jquery/dist/jquery.js');
+// require('../../vendors/jquery/dist/jquery.js');
+require('../../vendors/bootstrap/dist/js/bootstrap.js');
 require('../../vendors/bootstrap-switch/dist/js/bootstrap-switch.min.js');
 require('../js/page/projects.js');
 
-require('../../vendors/bootstrap/dist/js/bootstrap.min.js');
 // require('../../vendors/pnotify/dist/pnotify.js');
 require('../../vendors/Promise.prototype.finally/finally.js');
 require('../../vendors/sweetalert2/dist/sweetalert2.js');
