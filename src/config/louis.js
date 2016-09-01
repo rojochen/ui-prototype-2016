0173491948d5f2c94724ed857395090fd1a3a30f
Louis <neighborhood0830@gmail.com>
2016-09-01 12:08:55 +0800

    clean

--- src/config/louis.js
+++ src/config/louis.js
@@ -11,10 +11,10 @@ require('../../vendors/sweetalert2/dist/sweetalert2.js');
 
 // echarts2
 require('../../vendors/echarts/dist/echarts.min.js');
-require('../../vendors/echarts/dist/extension/dataTool.min.js');
+// require('../../vendors/echarts/dist/extension/dataTool.min.js');
 
 
 // page
 require('../js/page/projects.js');
 require('../js/page/notifications.js');
-require('../js/page/test.js');
+require('../js/page/echarts2.js');
