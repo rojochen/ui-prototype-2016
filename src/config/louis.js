a3c74cef7529d8a40c2bf4671f5ae836436243b1
Louis <neighborhood0830@gmail.com>
2016-09-02 10:05:55 +0800

    test

--- src/config/louis.js
+++ src/config/louis.js
@@ -1,5 +1,7 @@
 // projects
-// require('../../vendors/bootstrap-switch/dist/js/bootstrap-switch.min.js');
+// require('../../vendors/jquery/dist/jquery.js');
+require('../../vendors/bootstrap/dist/js/bootstrap.js');
+require('../../vendors/bootstrap-switch/dist/js/bootstrap-switch.min.js');
 
 // echarts2
 // require('../../vendors/echarts/dist/echarts.min.js');
@@ -7,9 +9,10 @@
 
 // notifications
 // require('../../vendors/pnotify/dist/pnotify.js');
+require('../../vendors/Promise.prototype.finally/finally.js');
 require('../../vendors/sweetalert2/dist/sweetalert2.js');
 
 // page
-// require('../js/page/projects.js');
+require('../js/page/projects.js');
 // require('../js/page/echarts2.js');
 require('../js/page/notifications.js');
