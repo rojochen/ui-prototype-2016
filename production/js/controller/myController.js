156e56a84a5d4e0d04242f5a9378d4810d179448
Roger Chen <rogerchen@bluetechnology.com.tw>
2016-09-13 12:43:43 +0800

    add controller&module

---
+++ production/js/controller/myController.js
@@ -0,0 +1,8 @@
+define([
+    '../module/myApp',
+], function(myApp) {
+    'use strict';
+    //myApp.controller()
+    
+    return myApp;
+});
---
+++ production/js/module/myModule.js
@@ -0,0 +1,8 @@
+define([
+    'pokemon',
+], function(pokemon) {
+    'use strict';
+    var app = angular.module('myModule',['pokemon']);
+     
+    return app;
+});
