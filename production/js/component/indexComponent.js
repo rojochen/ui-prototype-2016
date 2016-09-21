9ce1821a7091519d857331e50e92a80c07afcfca
WeijiaoJhao <1713161@gmail.com>
2016-09-21 14:19:01 +0800

    更新btPortlet為<

--- production/js/component/indexComponent.js
+++ production/js/component/indexComponent.js
@@ -5,12 +5,14 @@ define(['btModule'], function(btModule) {
     var indexComponent = {
         // template: '<p>function: 取得 {{vm.hero.name}}</p> <button ng-click="vm.onSayHello()">button</button>',
         // template: '<p>function: 取得 {{vm.hero.name}}</p> <button ng-click="vm.onclick()">button</button>',
-        template: '<p>function: 取得 {{vm.hero.name}}</p> <button ng-click="vm.onclick()">button</button> <aaa ng-click="vm.demo()">aaa</aaa>',
+        template: '<p>function: 取得 {{vm.hero.name}}</p> <button ng-click="vm.onclick()">{{vm.buttonName}}</button> <aaa ng-click="vm.demo()">aaa</aaa>',
         controller: indexComponentCtrl,
         controllerAs: 'vm',
+        transclude: true,
         bindings: {
             hero: '<',
-            onSayHello: '&'
+            onSayHello: '&',
+            buttonName: '<'
         }
     };
     app.component('indexComponent', indexComponent);
@@ -18,21 +20,27 @@ define(['btModule'], function(btModule) {
     indexComponentCtrl.$inject = ['$http'];
     function indexComponentCtrl($http){
         var vm = this;
-        vm.hero = {
-            name: 'index-component try'
-        };
-        /*
+        
+        vm.$onInit = function(){
+            vm.buttonName_1 = '第一層component button';
+            vm.buttonName = 'button';
+
+            vm.hero = {
+                name: 'index-component try'
+            };
+        }
+        
         vm.onSayHello = function(){
             alert('sayHello-aa');
-        };*/
+        };
         vm.onclick = function(){
             alert('sayHello-ee');
-            vm.onSayHello();
-             
+            vm.onSayHello(); 
         }
         vm.demo = function(){
             alert('demo component-1');
         }
+
     };
 
     return app;
