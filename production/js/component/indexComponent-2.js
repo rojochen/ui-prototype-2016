c62a47dcbb6ebd3c4ae03faed5bd4a91cbc8e60c
WeijiaoJhao <1713161@gmail.com>
2016-09-22 14:41:43 +0800

    parents chride component

--- production/js/component/indexComponent-2.js
+++ production/js/component/indexComponent-2.js
@@ -3,13 +3,14 @@ define(['btModule'], function(btModule) {
     var app = angular.module("btModule");
     
     var aaa = {
-        template: '<hr/><p>{{vm.name}}</p><button ng-click="vm.onDemo()">{{vm.button}}</button>',
+        template: '<hr/><p>{{vm.name}}</p><button ng-click="vm.onDemo()">{{vm.button}}</button><button ng-click="vm.change_2()">{{vm.button_2}}</button>',
         controller: indexComponentCtrl_2,
         controllerAs: 'vm',
         transclude: true,
         bindings: {
             onDemo: '&',
-            buttonName: '<'
+            buttonName: '<',
+            button: '<'
         },
         require: {
             parentCtrl: '^?indexComponent'
@@ -20,12 +21,29 @@ define(['btModule'], function(btModule) {
     indexComponentCtrl_2.$inject = ['$http'];
     function indexComponentCtrl_2($http){
         var vm = this;
-        vm.name = '第二層component區域';
+        vm.$onInit = function(){
+            vm.name = '第二層component區域';
+            vm.button_2 = 'change 第一層';
+        }
 
-        vm.button = '第二層component button';
         vm.onDemo = function(){
             alert('demo component-2');
+        }
+
+        vm.change_2 = function(){
             vm.parentCtrl.buttonName = 'change 第一層button';
+        }
+       
+        vm.$onChanges = function(obj){
+            console.log('2');
+            console.log(obj);
+            // console.log(obj.button.previousValue);
+            // console.log(obj.button.currentValue);
+            // console.log(obj.button.isFirstChange());
+        }
+
+        vm.$docheck = function(){
+            console.log('sss');
         }
     };
 
--- production/js/component/indexComponent.js
+++ production/js/component/indexComponent.js
@@ -5,7 +5,7 @@ define(['btModule'], function(btModule) {
     var indexComponent = {
         // template: '<p>function: 取得 {{vm.hero.name}}</p> <button ng-click="vm.onSayHello()">button</button>',
         // template: '<p>function: 取得 {{vm.hero.name}}</p> <button ng-click="vm.onclick()">button</button>',
-        template: '<p>function: 取得 {{vm.hero.name}}</p> <button ng-click="vm.onclick()">{{vm.buttonName}}</button> <aaa ng-click="vm.demo()">aaa</aaa>',
+        template: '<p>function: 取得 {{vm.hero.name}}</p> <button ng-click="vm.onclick()">{{vm.buttonName}}</button><button ng-click="vm.change()">change第二層</button> <aaa ng-click="vm.demo()" button="vm.button_2">aaa</aaa>',
         controller: indexComponentCtrl,
         controllerAs: 'vm',
         transclude: true,
@@ -22,24 +22,41 @@ define(['btModule'], function(btModule) {
         var vm = this;
         
         vm.$onInit = function(){
-            vm.buttonName_1 = '第一層component button';
-            vm.buttonName = 'button';
-
             vm.hero = {
                 name: 'index-component try'
             };
+            vm.buttonName = '第一層 button';
+            // vm.buttonName_1 = '第一層component button';
+            vm.button_2 = '第二層 button';
         }
-        
-        vm.onSayHello = function(){
-            alert('sayHello-aa');
-        };
+
         vm.onclick = function(){
             alert('sayHello-ee');
             vm.onSayHello(); 
         }
-        vm.demo = function(){
-            alert('demo component-1');
+        vm.onSayHello = function(){
+            alert('sayHello-aa');
+        };
+        vm.change = function(){
+            vm.button_2 = 'change 第二層component button';
         }
+        
+        vm.demo = function(){
+            // alert('demo component-1');
+        }
+
+        vm.$onChanges = function(obj){
+            console.log('1');
+            console.log(obj);
+            // console.log(obj.buttonName.previousValue);
+            // console.log(obj.buttonName.currentValue);
+            // console.log(obj.buttonName.isFirstChange());
+        }
+
+        vm.$docheck = function(){
+            console.log('aaa');
+        }
+
 
     };
 
