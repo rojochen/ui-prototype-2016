1b8d91d7a202b8ae9cd8e1164ca5f179d05a1693
WeijiaoJhao <1713161@gmail.com>
2016-12-06 13:04:08 +0800

    更新blockUI樣式

--- production/js/component/btPortlet.js
+++ production/js/component/btPortlet.js
@@ -8,8 +8,6 @@ define(['btModule'], function (btModule) {
         controllerAs: 'vm',
         transclude: true,
         bindings: {
-            fontclass: '@',
-            fabarsbtn: '<',
             titlename: '<',
             refreshstyle: '<',
             refreshbtn: '<',
@@ -20,29 +18,27 @@ define(['btModule'], function (btModule) {
     };
     app.component('btPortlet', btPortlet);
 
-    btPortletCtrl.$inject = ['$http','$element','$scope'];
-    function btPortletCtrl($http,$element,$scope) {
+    btPortletCtrl.$inject = ['$http', '$element', '$scope'];
+    function btPortletCtrl($http, $element, $scope) {
         var vm = this;
 
-        vm.$onInit = function(){
-            if(angular.isUndefined(vm.fabarsbtn)){
-                vm.fabarsbtn = false;
-            }
-            if(angular.isUndefined(vm.refreshbtn)){
+        vm.$onInit = function () {
+            if (angular.isUndefined(vm.refreshbtn)) {
                 vm.refreshbtn = true;
             }
-            if(angular.isUndefined(vm.switchbtn)){
+            if (angular.isUndefined(vm.switchbtn)) {
                 vm.switchbtn = true;
             }
-            if(angular.isUndefined(vm.selectbtn)){
+            if (angular.isUndefined(vm.selectbtn)) {
                 vm.selectbtn = true;
             }
-            if(angular.isUndefined(vm.closebtn)){
+            if (angular.isUndefined(vm.closebtn)) {
                 vm.closebtn = true;
             }
-            
-            // Panel toolbox
-            $element.on('click','.collapse-link', function () {
+
+
+            //版面縮和
+            $element.on('click', '.collapse-link', function () {
                 var $BOX_PANEL = $(this).closest('.x_panel'),
                     $ICON = $(this).find('i'),
                     $BOX_CONTENT = $BOX_PANEL.find('.x_content');
@@ -57,106 +53,107 @@ define(['btModule'], function (btModule) {
                 }
                 $ICON.toggleClass('fa-chevron-up fa-chevron-down');
             });
-            
+
+
+            //版面刪除
             $element.on('click', '.close-link', function () {
                 var $BOX_PANEL = $element.find(this).closest('.x_panel');
                 $element.remove();
             });
-            // Panel toolbox
 
-            // blockUI
+
+            //blockUI
             var isBlock;
             vm.blockUIStyle = {
                 mask: { message: null },
-                circles: { 
+                circles: {
                     message: '<img src="../images/default.svg" width="60%"/>',
                     // centerY: false,
-                    overlayCSS: { 
+                    overlayCSS: {
                         backgroundColor: '#ccc',
-                        opacity:0.2
+                        opacity: 0.2
                     },
-                    css:{
+                    css: {
                         border: 'none',
                         // top: '15%',
-                        background: 'transparent' 
-                    } 
+                        background: 'transparent'
+                    }
                 },
                 battery: {
-                  message: '<img src="../images/battery.svg" width="100px"/>',
-                //   centerY: false,
-                  overlayCSS: { 
+                    message: '<img src="../images/battery.svg" width="100px"/>',
+                    //   centerY: false,
+                    overlayCSS: {
                         backgroundColor: '#ccc',
-                        opacity:0.2
-                  },
-                  css:{
+                        opacity: 0.2
+                    },
+                    css: {
                         border: 'none',
                         // top: '30%',
-                        background: 'transparent' 
+                        background: 'transparent'
                     }
                 },
                 ball: {
-                  message: '<img src="../images/ball.svg" width="100px"/>',
-                //   centerY: false,
-                  overlayCSS: { 
+                    message: '<img src="../images/ball.svg" width="100px"/>',
+                    //   centerY: false,
+                    overlayCSS: {
                         backgroundColor: '#ccc',
-                        opacity:0.2
-                  },
-                  css:{
+                        opacity: 0.2
+                    },
+                    css: {
                         border: 'none',
                         // top: '30%',
-                        background: 'transparent' 
+                        background: 'transparent'
                     }
                 },
-                textPrompt: { 
+                textPrompt: {
                     message: '<h3><li class="fa fa-clock-o"></li> Please Ｗait...</h3>',
-                    css:{
-                        border: 'none', 
-                        padding: '15px', 
-                        backgroundColor: '#000', 
-                        '-webkit-border-radius': '10px', 
-                        '-moz-border-radius': '10px', 
-                        opacity: .5, 
-                        color: '#fff' 
+                    css: {
+                        border: 'none',
+                        padding: '15px',
+                        backgroundColor: '#000',
+                        '-webkit-border-radius': '10px',
+                        '-moz-border-radius': '10px',
+                        opacity: .5,
+                        color: '#fff'
                     }
                 }
             }
 
-            if(angular.isUndefined(vm.refreshstyle) || vm.refreshstyle === ''){
+            if (angular.isUndefined(vm.refreshstyle) || vm.refreshstyle === '') {
                 vm.blockUIStyleUrl = vm.blockUIStyle.mask;
             }
-            if(vm.refreshstyle === 'mask'){
+            if (vm.refreshstyle === 'mask') {
                 vm.blockUIStyleUrl = vm.blockUIStyle.mask;
             }
-            if(vm.refreshstyle === 'circles'){
+            if (vm.refreshstyle === 'circles') {
                 vm.blockUIStyleUrl = vm.blockUIStyle.circles;
             }
-            if(vm.refreshstyle === 'battery'){
+            if (vm.refreshstyle === 'battery') {
                 vm.blockUIStyleUrl = vm.blockUIStyle.battery;
             }
-            if(vm.refreshstyle === 'ball'){
+            if (vm.refreshstyle === 'ball') {
                 vm.blockUIStyleUrl = vm.blockUIStyle.ball;
             }
-            if(vm.refreshstyle === 'textPrompt'){
+            if (vm.refreshstyle === 'textPrompt') {
                 vm.blockUIStyleUrl = vm.blockUIStyle.textPrompt;
             }
 
-            $element.on('click','.refresh-link', function(){
-                // console.log(vm.refreshstyle);
+            $element.on('click', '.refresh-link', function () {
                 var _this = $(this).parents('.x_panel').find('.x_content'),
-                    unblock = function(){
+                    unblock = function () {
                         _this.unblock();
                     };
 
-                if(angular.isUndefined(isBlock)){
+                if (angular.isUndefined(isBlock)) {
                     isBlock = true;
-                }else{
+                } else {
                     isBlock = !isBlock;
                 }
                 // console.log(isBlock);
 
-                if(isBlock === true){
+                if (isBlock === true) {
                     _this.block(vm.blockUIStyleUrl);
-                }else{
+                } else {
                     unblock();
                 }
                 // setTimeout(unblock, 2000);   // 開關改為isBlock布林子控制
@@ -168,14 +165,14 @@ define(['btModule'], function (btModule) {
             $scope.$destroy();
         });
 
-        
-        vm.$onChanges = function(obj){
+
+        vm.$onChanges = function (obj) {
             // console.log(obj);
         };
 
-        vm.$onDestroy = function(){
-           //off event 
-           $element.off('click');
+        vm.$onDestroy = function () {
+            //off event 
+            $element.off('click');
         };
     };
 
