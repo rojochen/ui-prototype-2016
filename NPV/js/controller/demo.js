52b31e724f0d25165a1b881147536d0c545e86e3
yun1988 <small2883@hotmail.com>
2016-12-08 18:36:56 +0800

    更改為中文

--- NPV/js/controller/demo.js
+++ NPV/js/controller/demo.js
@@ -391,6 +391,29 @@ define(['btModule'], function (btModule) {
                 $log.debug('新增');
             }
             /*end 問卷-查詢*/
+
+        var opt = {
+            "oLanguage": {
+                "sProcessing": "處理中...",
+                "sLengthMenu": "顯示 _MENU_ 項結果",
+                "sZeroRecords": "沒有匹配結果",
+                "sInfo": "顯示第 _START_ 至 _END_ 項結果，共 _TOTAL_ 項",
+                "sInfoEmpty": "顯示第 0 至 0 項結果，共 0 項",
+                "sInfoFiltered": "(從 _MAX_ 項結果過濾)",
+                "sSearch": "搜索:",
+                "oPaginate": {
+                    "sFirst": "首頁",
+                    "sPrevious": "上頁",
+                    "sNext": "下頁",
+                    "sLast": "尾頁"
+                },
+                bFilter: false
+            }
+        };
+        $("#datatable").DataTable(opt);
+        $("#datatable_activities").DataTable(opt);
+
+
     }]);
     return app;
 });
