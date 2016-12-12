584713c0a6c382c519cb07b3efd604032f9b0077
WeijiaoJhao <1713161@gmail.com>
2016-12-12 19:04:40 +0800

    layer.open

--- production/js/component/indexComponent.js
+++ production/js/component/indexComponent.js
@@ -63,7 +63,42 @@ define(['btModule'], function (btModule) {
         layer.config({
             path: '/production/js/lib/layer/src/' //layer.js所在的目录，可以是绝对目录，也可以是相对目录
         });
-        layer.alert('见到你真的很高兴sss', { icon: 6 });
+        // layer.alert('见到你真的很高兴sss', { icon: 6 });
+        //页面层
+        layer.open({
+            type: 1,
+            title: '我是標題',
+            // skin: 'layer-ext-moon', //自訂的class名稱
+            // area: ['420px', '240px'], //宽高
+            maxmin: true,
+            shadeClose: true, //开启遮罩关闭
+            zIndex: 10,  //zIndex - 层叠顺序(默认：19891014)
+            content: '我是html内容',
+            btn: ['按钮一', '按钮二'],
+            // btnAlign: 'c',  //btnAlign - 按钮排列(l、c、r)
+            // closeBtn:1,  //closeBtn - 关闭按钮
+            yes: function (index, layero) {  //yes - 确定按钮回调方法
+                //按钮【按钮一】的回调
+                console.log('a');
+            }, btn2: function (index, layero) {
+                //按钮【按钮二】的回调
+                console.log('b');
+            },
+            cancel: function () {  //cancel - 右上角关闭按钮触发的回调
+                console.log('右上角关闭回调');  //右上角关闭回调
+            },
+            full:function () {
+                console.log('最大化');  //最大化
+            },
+            min:function () {
+                console.log('最小化');  //最小化
+            },
+            restore:function () {
+                console.log('还原');  //还原
+            }
+        });
+
+
 
     };
 
