131dc3d65d13ce89d2023813c7294ff4dbc23b44
WeijiaoJhao <1713161@gmail.com>
2016-12-13 16:36:24 +0800

    加浮動視窗內容

--- NPV/js/component/header.js
+++ NPV/js/component/header.js
@@ -31,7 +31,71 @@ define([
                     offset: 'rb',
                     shade: 0,
                     zIndex: 2,
-                    content: '我是html内容'
+                    content: `
+                    <ul class="list-group">
+									<li class="clearfix listArea">
+										<div style="width:100%" class="cart-list-title bg_green">
+											 	活動類別
+										</div>
+										<div class="listText">
+											<span class="activityName">活動代碼：A2696</span>
+											<span class="activityName">活動類別：3轉4_iPhone SE 絕配續約專案_預繳(測試)</span>
+										</div>
+										<div class="listBottom">
+											<div class="iconArea">
+												<i class="fa fa-pencil"></i>編輯
+											</div>
+											<div class="iconArea">
+												<i class="fa fa fa-eye"></i>檢視
+											</div>
+											<div class="iconArea">
+												<i class="fa fa-trash-o"></i>移除
+											</div>
+										</div>
+									</li>
+
+									<li class="clearfix listArea">
+										<div style="width:100%" class="cart-list-title bg_blue">
+											 	促案類別
+										</div>
+										<div class="listText">
+											<span class="activityName">活動代碼：A2696</span>
+											<span class="activityName">活動類別：3轉4_iPhone SE 絕配續約專案_預繳(測試)</span>
+										</div>
+										<div class="listBottom">
+											<div class="iconArea">
+												<i class="fa fa-pencil"></i>編輯
+											</div>
+											<div class="iconArea">
+												<i class="fa fa fa-eye"></i>檢視
+											</div>
+											<div class="iconArea">
+												<i class="fa fa-trash-o"></i>移除
+											</div>
+										</div>
+									</li>
+
+									<li class="clearfix listArea">
+										<div style="width:100%" class="cart-list-title bg_pink">
+											 	折扣類別
+										</div>
+										<div class="listText">
+											<span class="activityName">活動代碼：A2696</span>
+											<span class="activityName">活動類別：3轉4_iPhone SE 絕配續約專案_預繳(測試)</span>
+										</div>
+										<div class="listBottom">
+											<div class="iconArea">
+												<i class="fa fa-pencil"></i>編輯
+											</div>
+											<div class="iconArea">
+												<i class="fa fa fa-eye"></i>檢視
+											</div>
+											<div class="iconArea">
+												<i class="fa fa-trash-o"></i>移除
+											</div>
+										</div>
+									</li>
+								</ul>`
                 });
             } else {
                 layer.restore(vm.layerList);
