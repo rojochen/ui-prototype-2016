46d6626b135e7da3fca6835e05d4000bc97c5bf0
yun1988 <small2883@hotmail.com>
2016-12-21 16:49:35 +0800

    導頁

--- NPV/js/module/demoModule.js
+++ NPV/js/module/demoModule.js
@@ -1,19 +1,24 @@
-define(['pokemon'], function(pokemon) {
-    'use strict';
-    var app = angular.module('btModule',['pokemon']);
-    // 導頁路徑
-    app.config(['$routeProvider', function ($routeProvider) {
+define(['pokemon'], function (pokemon) {
+	'use strict';
+	var app = angular.module('btModule', ['pokemon']);
+	// 導頁路徑
+	app.config(['$routeProvider', function ($routeProvider) {
 		$routeProvider
-				.when('/', {
-					templateUrl: "../NPV/views/demo_1.html"
-				})
-				.when('/try', {
-					templateUrl: "../NPV/views/try.html"
-				})
-				.otherwise({
-					redirectTo: '/'
-				});
-		}]);
+			.when('/', {
+				templateUrl: "../NPV/views/demo_1.html"
+			})
+			.when('/try', {
+				templateUrl: "../NPV/views/try.html"
+			})
+			.when('/:id', {
+				templateUrl: '../NPV/views/demo_2.html',
 
-    return app;
+			})
+
+		.otherwise({
+			redirectTo: '/'
+		});
+	}]);
+
+	return app;
 });
