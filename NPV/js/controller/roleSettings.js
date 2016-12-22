e6e8a57b52d2a9e09c72b93f5bf9f40cb27a7f37
yun1988 <small2883@hotmail.com>
2016-12-22 16:48:33 +0800

    增加modal

--- NPV/js/controller/roleSettings.js
+++ NPV/js/controller/roleSettings.js
@@ -117,129 +117,84 @@ define(['btModule'], function (btModule) {
             $scope.names6 = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9"];
             $scope.names7 = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9"];
 
-            $scope.activitiesTableData = [{
-                "gateType": "FET",
-                "sort": "3轉4",
-
+            $scope.roleTableData = [{
+                "roleId": "1",
+                "roleName": "System Admin",
             }, {
-                "gateType": "AAA",
-                "sort": "3轉4",
+                "roleId": "2",
+                "roleName": "PM",
             }, {
-
-                "gateType": "AAA",
-                "sort": "3轉4",
-
+                "roleId": "3",
+                "roleName": "Team Leader",
             }, {
-                "gateType": "AAA",
-                "sort": "3轉4",
+                "roleId": "4",
+                "roleName": "Team Member",
             }, {
-                "NO": "05",
-                "gateType": "AAA",
-                "sort": "3轉4",
-                "activityCode": "E3333",
-                "activityName": "3轉4_iPhone SE 絕配續約專案_預繳",
-                "activityRange": "2016/09/08-2016/12/31",
-                "groupCode": "G003",
-                "groupName": "G0141-4G續約-iPhone預繳方案",
-                "useOfPersonnel": " N",
-                "pmOwner": "pm01",
-                "fileEffectiveDate": "2016/09/09",
-                "stateEffective": "活動設定中，上傳IA等待排程",
-                "downtimeCanRate": "高",
-                "arpb": "0-999",
-                "expirationData": "93",
-                "relationship": "",
-                "species": "",
-                "item": "",
-                "pm": "",
-            }, {
-                "gateType": "AAA",
-                "sort": "3轉4",
-
-            }, {
-                "gateType": "AAA",
-                "sort": "3轉4",
+                "roleId": "5",
+                "roleName": "System Admin",
             }, ];
 
-            $scope.showCommonTable = true;
-            $('#datatable').DataTable().destroy();
+            // $scope.showCommonTable = true;
+            // $('#datatable').DataTable().destroy();
 
-            $timeout(function () {
-                $('#datatable').DataTable(opt);
-                // $log.debug($scope.saerchKeyInput);
-                $scope.saerchKey = $scope.saerchKeyInput;
-            }, 100)
-
-
-            $scope.showActivities = false;
-            $scope.saerchAdvanced = function () {
-                $('#datatable').DataTable().destroy();
-                $scope.commonTableData = [];
-                $scope.showCommonTable = false;
-                $scope.showSearch = false;
-                $log.debug($scope.selectType);
-                if ($scope.selectType === $scope.selectTypeItem[0]) {
-                    $timeout(function () {
-                        $("#activityGroup").select2({
-                            placeholder: "選擇活動群組",
-                            allowClear: true
-                        });
-                        $("#numberType").select2({
-                            placeholder: "選擇門號類型",
-                            allowClear: true
-                        });
-                        $("#type").select2({
-                            placeholder: "選擇種類",
-                            allowClear: true
-                        });
-                    }, 100)
-                    $scope.showActivities = true;
-                } else {
-                    $log.debug('next'); //未做...完
-                }
-                // $log.debug($scope.commonTableData);
-            }
-
-            $scope.showActivitiesTable = false;
-            $scope.activities_Advanced = function () { //未做...完
-                $log.debug('進階查詢_活動');
-                $log.debug($scope.SPV + " " + $scope.activityCode + " " + $scope.activityName + " " + $scope.groupCode + " " + $scope.activityGroup + " " + $scope.numberType + " " + $scope.type);
-                $timeout(function () {
-                    $('#datatable_activities').DataTable(opt);
-                    // $scope.saerchKey = $scope.saerchKeyInput;  //search未做...
-                }, 100)
-                $log.debug($scope.activitiesTableData);
-                $scope.showActivitiesTable = true;
-            }
-
-            $scope.changeSearch = function () {
-                $scope.showSearch = true;
-                $scope.showActivities = false;
-            }
+            // $timeout(function () {
+            //     $('#datatable').DataTable(opt);
+            //     // $log.debug($scope.saerchKeyInput);
+            //     $scope.saerchKey = $scope.saerchKeyInput;
+            // }, 100)
 
 
+            // $scope.showActivities = false;
+            // $scope.saerchAdvanced = function () {
+            //     $('#datatable').DataTable().destroy();
+            //     $scope.commonTableData = [];
+            //     $scope.showCommonTable = false;
+            //     $scope.showSearch = false;
+            //     $log.debug($scope.selectType);
+            //     if ($scope.selectType === $scope.selectTypeItem[0]) {
+            //         $timeout(function () {
+            //             $("#activityGroup").select2({
+            //                 placeholder: "選擇活動群組",
+            //                 allowClear: true
+            //             });
+            //             $("#numberType").select2({
+            //                 placeholder: "選擇門號類型",
+            //                 allowClear: true
+            //             });
+            //             $("#type").select2({
+            //                 placeholder: "選擇種類",
+            //                 allowClear: true
+            //             });
+            //         }, 100)
+            //         $scope.showActivities = true;
+            //     } else {
+            //         $log.debug('next'); //未做...完
+            //     }
+            //     // $log.debug($scope.commonTableData);
+            // }
 
-            // it參數設定
-            /* begin */
-            $scope.btnShow1 = true;
-            $scope.btnShow2 = false;
-            $scope.advancedSearch = function () {
-                    if ($scope.advancedControl) {
-                        $scope.advancedControl = false;
-                        $scope.btnShow1 = true;
-                        $scope.btnShow2 = false;
-                    } else {
-                        $scope.advancedControl = true;
-                        $scope.btnShow1 = false;
-                        $scope.btnShow2 = true;
-                    }
-                }
-                /* end */
+            // $scope.showActivitiesTable = false;
+            // $scope.activities_Advanced = function () { //未做...完
+            //     $log.debug('進階查詢_活動');
+            //     $log.debug($scope.SPV + " " + $scope.activityCode + " " + $scope.activityName + " " + $scope.groupCode + " " + $scope.activityGroup + " " + $scope.numberType + " " + $scope.type);
+            //     $timeout(function () {
+            //         $('#datatable_activities').DataTable(opt);
+            //         // $scope.saerchKey = $scope.saerchKeyInput;  //search未做...
+            //     }, 100)
+            //     $log.debug($scope.activitiesTableData);
+            //     $scope.showActivitiesTable = true;
+            // }
+
+            // $scope.changeSearch = function () {
+            //     $scope.showSearch = true;
+            //     $scope.showActivities = false;
+            // }
+
 
             /* begin */
-            $scope.tableControl = false;
-            $scope.showTable = function () {
-                    $scope.tableControl = true;
+            $scope.multiselectControl = false;
+            $scope.showMultiselect = function () {
+                    $scope.multiselectControl = true;
                 }
                 /* end */
 
