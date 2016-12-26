7c010c36cdab1958e346cc89933461d4e18a1693
yun1988 <small2883@hotmail.com>
2016-12-26 18:54:50 +0800

    修改為有面板的底

--- NPV/js/controller/ITParameterSet.js
+++ NPV/js/controller/ITParameterSet.js
@@ -12,6 +12,24 @@ define(['btModule'], function (btModule) {
         function ($scope, $timeout, $log, $element, shoppingCartEntity) {
             var vm = this;
 
+            /*begin 版面縮合*/
+            $element.on('click', '.collapse-link', function () {
+                var $BOX_PANEL = $(this).closest('.x_panel'),
+                    $ICON = $(this).find('i'),
+                    $BOX_CONTENT = $BOX_PANEL.find('.x_content');
+
+                if ($BOX_PANEL.attr('style')) {
+                    $BOX_CONTENT.slideToggle(200, function () {
+                        $BOX_PANEL.removeAttr('style');
+                    });
+                } else {
+                    $BOX_CONTENT.slideToggle(200);
+                    $BOX_PANEL.css('height', 'auto');
+                }
+                $ICON.toggleClass('fa-minus  fa-plus');
+            });
+            /*end 版面縮合*/
+
 
             // it參數設定
             /* begin */
@@ -37,11 +55,6 @@ define(['btModule'], function (btModule) {
                 }
                 /* end */
 
-            /* begin */
-            $scope.resetTable = function () {
-                    $scope.tableControl = false;
-                }
-                /* end */
         }
     ]);
     return app;
