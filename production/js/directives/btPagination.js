d5b10a2c4fe9e860410792d82bfeb50de9787787
WeijiaoJhao <1713161@gmail.com>
2017-01-04 13:39:38 +0800

    改讀x

--- production/js/directives/btPagination.js
+++ production/js/directives/btPagination.js
@@ -5,9 +5,11 @@ define(['btModule'], function (btModule) {
     app.directive('btPagination', function(){
         
         function link(scope, element, attrs){
-            var pageSize = Number.parseInt(attrs['pageSize']);
+            var pageSize = Number.parseInt(attrs['pageSize']),
+                pageCount = Number.parseInt(attrs['pageCount']);
             // console.log(scope.info.totalCount);
             // console.log(pageSize);
+            console.log(pageCount);  //未...
 
             scope.pageArray = [];
             var endPage = Math.ceil(scope.info.totalCount/pageSize);
@@ -15,38 +17,36 @@ define(['btModule'], function (btModule) {
             for (var num = 1; num <= endPage;num++){
                 scope.pageArray.push(num);
             }
-            // console.log(scope.pageArray);
+            console.log(scope.pageArray);
             
-            // console.log(attrs['pageCount']);  //未...
-
-            scope.changePage = function(index){
-                console.log(index);
-                scope.page = index;
+            scope.changePage = function(x){
+                console.log(x);
+                scope.page = x;
                 // console.log(pageSize);
-                scope.startList = index*pageSize+1;
-                scope.endList = (scope.info.totalCount < (index+1)*pageSize)?scope.info.totalCount:(index+1)*pageSize;
+                scope.startList = (x-1)*pageSize+1;
+                scope.endList = (scope.info.totalCount < x*pageSize)?scope.info.totalCount:x*pageSize;
 
             }
-            scope.changePage(0);
+            scope.changePage(1);
 
             scope.changeFirst = function(){
-                scope.changePage(0);
+                scope.changePage(1);
             }
 
             scope.changePre = function(){
-                if(scope.page-1 >= 0){
+                if(scope.page-1 > 0){
                     scope.changePage(scope.page-1);
                 }
             }
 
             scope.changeNext = function(){
-                if(scope.page+1 <= scope.pageArray.length-1){
+                if(scope.page+1 <= scope.pageArray.length){
                     scope.changePage(scope.page+1);
                 }
             }
 
             scope.changeLast = function(){
-                scope.changePage(scope.pageArray.length-1);
+                scope.changePage(scope.pageArray.length);
             }
         }
 
@@ -58,11 +58,11 @@ define(['btModule'], function (btModule) {
             },
             link: link,
             template: `<ul class="pagination">
-						<li ng-class="{'disabled': page === 0}"><a href ng-click="changeFirst()">« 第一頁</a></li>
-						<li ng-class="{'disabled': page === 0}"><a href ng-click="changePre()">‹上一頁 </a></li>
-						<li ng-class="{'active': $index === page}" ng-repeat="x in pageArray track by $index"> <a href ng-bind="x" ng-click="changePage($index)"></a> </li>
-						<li ng-class="{'disabled': page === pageArray.length-1}"><a href ng-click="changeNext()">下一頁 ›</a></li>
-						<li ng-class="{'disabled': page === pageArray.length-1}"><a href ng-click="changeLast()">最後一頁 »</a></li>
+						<li ng-class="{'disabled': page === 1}"><a href ng-click="changeFirst()">« 第一頁</a></li>
+						<li ng-class="{'disabled': page === 1}"><a href ng-click="changePre()">‹上一頁 </a></li>
+						<li ng-class="{'active': $index === page-1}" ng-repeat="x in pageArray track by $index"> <a href ng-bind="x" ng-click="changePage(x)"></a> </li>
+						<li ng-class="{'disabled': page === pageArray.length}"><a href ng-click="changeNext()">下一頁 ›</a></li>
+						<li ng-class="{'disabled': page === pageArray.length}"><a href ng-click="changeLast()">最後一頁 »</a></li>
 					</ul>
                     <p>顯示第{{startList}}筆至第{{endList}}筆，共有{{info.totalCount}}筆</p>`
         };
