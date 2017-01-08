5ebda25f4dc7f4e5c270a96b5d275f87f11e3176
yvonne6344 <yvonne791016@gmail.com>
2017-01-09 02:41:07 +0800

    促銷活動類別 畫面OK

--- NPV/js/controller/activityClassification.js
+++ NPV/js/controller/activityClassification.js
@@ -36,26 +36,100 @@ define(['btModule'], function (btModule) {
             $scope.btnShow1 = true;
             $scope.btnShow2 = false;
             $scope.advancedSearch = function () {
-                    if ($scope.advancedControl) {
-                        $scope.advancedControl = false;
-                        $scope.btnShow1 = true;
-                        $scope.btnShow2 = false;
-                    } else {
-                        $scope.advancedControl = true;
-                        $scope.btnShow1 = false;
-                        $scope.btnShow2 = true;
-                    }
+                if ($scope.advancedControl) {
+                    $scope.advancedControl = false;
+                    $scope.btnShow1 = true;
+                    $scope.btnShow2 = false;
+                } else {
+                    $scope.advancedControl = true;
+                    $scope.btnShow1 = false;
+                    $scope.btnShow2 = true;
                 }
-                /* end */
+            };
+            /* end */
+
+            /* 促銷活動類別表格 begin */
+            var opt = {
+                "oLanguage": {
+                    "sProcessing": "處理中...",
+                    "sLengthMenu": "顯示 _MENU_ 項結果",
+                    "sZeroRecords": "沒有匹配結果",
+                    "sInfo": "顯示第 _START_ 至 _END_ 項結果，共 _TOTAL_ 項",
+                    "sInfoEmpty": "顯示第 0 至 0 項結果，共 0 項",
+                    "sInfoFiltered": "(從 _MAX_ 項結果過濾)",
+                    "sSearch": "搜索:",
+                    "oPaginate": {
+                        "sFirst": "首頁",
+                        "sPrevious": "上一頁",
+                        "sNext": "下一頁",
+                        "sLast": "尾頁"
+                    },
+                },
+                "searching": false,
+                "bInfo": false,
+                "bPaginate": false
+                    // "scrollX": true,
+                    // "scrollY": true
+            };
+
+            $timeout(function () {
+                $('#datatable_activityClassification').DataTable(opt);
+            }, 100)
+
+            // 表格內容資料
+            $scope.activityClassificationTableData = [{
+                "activityClassificationSorting": "1",
+                "activityClassificationID": "01",
+                "activityClassificationName": "【單門號無約專案】",
+                "activityClassificationRole": "ejchen",
+                "activityClassificationDate": "23-9月 -16"
+            }, {
+                "activityClassificationSorting": "2",
+                "activityClassificationID": "02",
+                "activityClassificationName": "【手機/其他類商品專案】",
+                "activityClassificationRole": "SPV",
+                "activityClassificationDate": "08-3月 -10"
+            }, {
+                "activityClassificationSorting": "3",
+                "activityClassificationID": "03",
+                "activityClassificationName": "【月租折扣/通話優惠/禮品類專案】",
+                "activityClassificationRole": "SPV",
+                "activityClassificationDate": "08-3月 -10"
+            }, {
+                "activityClassificationSorting": "4",
+                "activityClassificationID": "04",
+                "activityClassificationName": "【大寬頻專案】",
+                "activityClassificationRole": "SPV",
+                "activityClassificationDate": "08-3月 -10"
+            }, {
+                "activityClassificationSorting": "5",
+                "activityClassificationID": "05",
+                "activityClassificationName": "【WiMAX專案】",
+                "activityClassificationRole": "SPV",
+                "activityClassificationDate": "08-3月 -10"
+            }, {
+                "activityClassificationSorting": "6",
+                "activityClassificationID": "06",
+                "activityClassificationName": "無",
+                "activityClassificationRole": "SPV",
+                "activityClassificationDate": "08-3月 -10"
+            }, {
+                "activityClassificationSorting": "7",
+                "activityClassificationID": "07",
+                "activityClassificationName": "【Activation Team 專案】",
+                "activityClassificationRole": "SPV",
+                "activityClassificationDate": "08-3月 -10"
+            }];
+
+            $('#datatable_activityClassification').DataTable().destroy();
+            /* 促銷活動類別表格 end */
 
             /* begin */
             $scope.tableControl = false;
             $scope.showTable = function () {
-                    $scope.tableControl = true;
-                }
-                /* end */
-
-
+                $scope.tableControl = true;
+            };
+            /* end */
         }
     ]);
     return app;
